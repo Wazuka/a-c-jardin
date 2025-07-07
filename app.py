3eed1baffed8a7346e1d6dbe820031abd8671ee3
Wazuka <romain.venet@gmail.com>
2025-07-07 10:34:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,8 @@ if not st.session_state.entry_written:
             }
         )
             st.session_state.entry_written = True
-            st.success(f"{message}<br><i>Heure enregistrée : {now_str}</i>", unsafe_allow_html=True)
+            st.markdown(f"<div style='background-color:#eaf6ec; padding:0.5em 1em; border-radius:8px;'>"
+            f"{message}<br><i>Heure enregistrée : {now_str}</i></div>", unsafe_allow_html=True)
 
 else:
     st.info("Entrée déjà enregistrée aujourd'hui.")
